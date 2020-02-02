26f3ab9763a8b0c43cc07fa90a66049f48aa67bb
Chua Hou <human+github@chuahou.dev>
2020-02-02 15:21:49 +0000

    Set up alarm activity, receiver and service

---
+++ app/src/main/java/dev/chuahou/amadeusalarm/AlarmActivity.java
@@ -0,0 +1,16 @@
+package dev.chuahou.amadeusalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlarmActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
---
+++ app/src/main/java/dev/chuahou/amadeusalarm/AlarmService.java
@@ -0,0 +1,19 @@
+package dev.chuahou.amadeusalarm;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AlarmService extends Service
+{
+    public AlarmService()
+    {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
