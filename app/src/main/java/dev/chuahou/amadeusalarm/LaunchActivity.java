5f5b36c46ba04161791b28dfa906b1e2a4a3d606
Chua Hou <human+github@chuahou.dev>
2020-02-02 20:40:45 +0000

    Remove title bar from LaunchActivity

--- app/src/main/java/dev/chuahou/amadeusalarm/LaunchActivity.java
+++ app/src/main/java/dev/chuahou/amadeusalarm/LaunchActivity.java
@@ -1,10 +1,9 @@
 package dev.chuahou.amadeusalarm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class LaunchActivity extends AppCompatActivity
+public class LaunchActivity extends Activity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
