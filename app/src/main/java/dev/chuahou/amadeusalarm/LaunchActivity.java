26c7b3403d7337a8b5e97a35049ae22d4ddd5a33
Chua Hou <human+github@chuahou.dev>
2020-02-02 20:31:16 +0000

    Add LaunchActivity layout from Yink's project and replace Amadeus icon

---
+++ app/src/main/java/dev/chuahou/amadeusalarm/LaunchActivity.java
@@ -0,0 +1,15 @@
+package dev.chuahou.amadeusalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
